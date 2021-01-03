b4f367233f3c2babc0c19ba93575c163c9aa3a30
AnsonAMS <anson_tohata@hotmail.com>
2021-01-03 14:30:27 +0100

    Adding date fns

--- sandbox.js
+++ sandbox.js
@@ -1,12 +1,55 @@
 const clock = document.querySelector('.clock');
 
+const addZeroToHour = (h) => {
+    if (h < 10) {
+      h = "0" + h;
+    }
+    return h;
+  }
+
+const addZeroToMinute = (m) => {
+    if (m < 10) {
+      m = "0" + m;
+    }
+    return m;
+  }  
+
+const addZeroToSecond = (s) => {
+    if ( s < 10) {
+       s = "0" + s;
+    }
+    return s;
+  }
 const tick = () => {
     
     const now = new Date();
     
     const h = now.getHours();
+
+    // const addZeroToHour = (h) => {
+    //     if (h < 10) {
+    //       h = "0" + h;
+    //     }
+    //     return h;
+    //   }
+
     const m = now.getMinutes();
+
+    // const addZeroToMinute = (m) => {
+    //     if (m < 10) {
+    //       m = "0" + m;
+    //     }
+    //     return m;
+    //   }
+
     const s = now.getSeconds();
+
+    // const addZeroToSecond = (s) => {
+    //     if ( s < 10) {
+    //        s = "0" + s;
+    //     }
+    //     return s;
+    //   }
 
     const html = `
     <span>${h}</span> :
@@ -18,4 +61,8 @@ const tick = () => {
 
 };
 
-setInterval(tick, 1000);
+setInterval(tick, 1000); 
+
+const now = new Date();
+
+console.log(dateFns.isToday(now));
